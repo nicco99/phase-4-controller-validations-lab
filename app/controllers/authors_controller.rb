4efa9dbff83425f1d4faf2d051c0fcc24b7b04c3
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-23 21:04:27 +0300

    completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,35 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  end
+  def update
+    author = Author.find(params[:id])
+    author.update!(author_params)
+    render json: author, status: :ok
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  # def render_not_found_response
+  #   render json: { error: "author not found" }, status: :not_found
+  # end
+
+
+  def render_unprocessable_entity_response invalid
+    render json: { errors: invalid.record.errors}, status: :unprocessable_entity
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+   validates :category, inclusion: ["Fiction","Non-Fiction"]
+
+ 
 end
